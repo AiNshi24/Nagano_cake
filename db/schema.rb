aecf372dcf573dea5d4241b58118b08b40c6cecd
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-07 12:00:51 +0000

    [Add]Addressモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_06_224603) do
+ActiveRecord::Schema.define(version: 2023_02_07_114804) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "last_name", null: false
