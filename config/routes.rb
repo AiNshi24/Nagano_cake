15c7aca88473e07b6543502da659cf0af562047f
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-08 12:37:45 +0000

    [Add]Publicコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  
+  
   devise_for :admins
   devise_for :customers
+  
+  scope module: :public do
   root to: "homes#top"
+  resources :items, only: [:index, :show]
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+  resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  get '/about' => 'homes#about'
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
