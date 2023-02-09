295bb72314613cebd73a1b8338dbb802694a8136
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-09 11:54:29 +0000

    [Add]Adminコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  
-  
-  devise_for :admins
+
   devise_for :customers
+  devise_for :admins
   
+  namespace :admin do
+    get '/admin' => 'admin/homes#top'
+    resources :items, only: [:new, :index, :create, :show, :edit, :update]
+    resources :genres, only: [:indax, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
   scope module: :public do
   root to: "homes#top"
   resources :items, only: [:index, :show]
@@ -14,6 +22,6 @@ Rails.application.routes.draw do
   resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   get '/about' => 'homes#about'
-  end 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
