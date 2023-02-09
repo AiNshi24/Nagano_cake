612822a8af46269a81f7a9eb1d54c8f662189675
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-09 11:54:10 +0000

    [Add]Adminコントローラ

--- db/migrate/20230209105811_add_devise_to_customers.rb
+++ db/migrate/20230209105811_add_devise_to_customers.rb
@@ -34,19 +34,7 @@ class AddDeviseToCustomers < ActiveRecord::Migration[6.1]
 
 
       # Uncomment below if timestamps were not included in your original model.
-      t.string :last_name, null: false
-      t.string :first_name, null: false
-      t.string :last_name_kana, null: false
-      t.string :first_name_kana, null: false
-    
-    
-      t.string :postal_code, null: false
-      t.string :address, null: false
-      t.string :telephone_number, null: false
-      t.boolean :is_deleted, null: false, default: "false"
-      
-      
-      t.timestamps null: false
+      # t.timestamps null: false
     end
 
     add_index :customers, :email,                unique: true
