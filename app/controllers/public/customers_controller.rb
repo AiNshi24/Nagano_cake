061d93df88f9ea5929d22779ed64c8a0310acc23
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-12 11:15:56 +0000

    [Add]管理者ログイン

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,5 +12,10 @@ class CustomersController < ApplicationController
   end
 
   def withdraw
+    @customer = Customer.find(params[:customer])
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to new_customer_registration
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,19 +4,19 @@ class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 before_action :customer_state, only: [:create]
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+   #def new
+     #super
+   #end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   def create
+     super
+   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+   def destroy
+     super
+   end
 
   protected
 
@@ -24,7 +24,7 @@ before_action :customer_state, only: [:create]
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
+
 # 退会しているかを判断するメソッド
   def customer_state
     ## 【処理内容1】 入力されたemailからアカウントを1件取得
@@ -32,10 +32,10 @@ before_action :customer_state, only: [:create]
     ## アカウントを取得できなかった場合、このメソッドを終了する
   return if !@customer
     # 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-  
+
     if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted == true
       redirect_to new_customer_registration
-    end 
+    end
   end
-  
+
 end
