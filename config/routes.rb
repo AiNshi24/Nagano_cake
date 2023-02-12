fd0a3b27a7a03e7b6dd73d1bfcc332ac6af454b0
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-12 11:16:10 +0000

    [Add]管理者ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,18 @@ Rails.application.routes.draw do
     registrations: "public/regstrations",
     sessions: 'public/sessions'
   }
-  devise_for :admins,skip: [:registrations, :passwords], controllers: {
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
-  
+
   namespace :admin do
-    get '/admin' => 'admin/homes#top'
+    get '/' => 'homes#top'
     resources :items, only: [:new, :index, :create, :show, :edit, :update]
     resources :genres, only: [:indax, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
+    resources :sessions, only: [:new, :create, :destroy]
   end
 
   scope module: :public do
@@ -22,11 +23,15 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show]
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
   resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
   resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   get '/about' => 'homes#about'
+  get 'customers/mypage' => 'customers#show'
+  get 'customers/information/edit' => 'customers#edit'
+  patch 'customers/information' => 'customers#update'
+  get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+  patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
