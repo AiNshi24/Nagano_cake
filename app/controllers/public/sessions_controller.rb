7b3280fc93c9af16a467326c12e377602f6a1c7e
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-13 12:31:03 +0000

    [Update]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -9,16 +9,22 @@ before_action :customer_state, only: [:create]
    #end
 
   # POST /resource/sign_in
-   def create
-     super
-   end
+  # def create
+  #   super
+  # end
 
   # DELETE /resource/sign_out
-   def destroy
-     super
-   end
+  # def destroy
+  #   super
+  # end
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
-  protected
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
