e2c74d25c0e2c839a3918f1cb57c6baecd76fbda
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-17 12:59:26 +0000

    [Add]商品式登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    item.save
+    redirect_to admin_items_path(@item[:id])
   end
 
   def show
@@ -16,4 +20,9 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+end
+
+private
+def item_params
+  params.require(:item).permit(:image, :name, :introduction, :price, :is_active)
 end
