37b566cd2961ed0add8c3424df7b8dfe961740a2
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-18 12:53:34 +0000

    [Add]商品詳細画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,7 +10,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     item.save
-    redirect_to admin_items_path(@item.id)
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
@@ -18,13 +18,15 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
   end
-end
 
-private
-def item_params
-  params.require(:item).permit(:image, :name, :introduction, :price, :is_active)
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :is_active)
+  end
 end
