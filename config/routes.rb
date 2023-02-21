1e24a2a2224f9fdba58d7a1e65ddbfc9b83bd903
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-21 13:20:39 +0000

    [Add]会員ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/regstrations",
+    registrations: "public/registrations",
     sessions: 'public/sessions'
   }
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
