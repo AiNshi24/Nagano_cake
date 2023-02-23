6c3e627f1f36bc978352772bacd767f35e4ce1d7
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-02-23 12:44:21 +0000

    [Add]ItemId,CustomerId column to CartItems table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_15_230740) do
+ActiveRecord::Schema.define(version: 2023_02_23_114344) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2023_02_15_230740) do
     t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "item_id"
+    t.integer "customer_id"
   end
 
   create_table "customers", force: :cascade do |t|
